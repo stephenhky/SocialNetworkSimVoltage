f2b8f8b5f50081cd661be6e72d82b554c82978b7
Kwan-Yuet Ho, Ph.D. <stephenhky@yahoo.com.hk>
2013-06-18 17:46:28 -0300

    Update GraphSimVol.py

--- GraphSimVol.py
+++ GraphSimVol.py
@@ -39,54 +39,56 @@ class GraphSimVoltage:
         matB = np.matrix(np.zeros([len(self.edges)+1, len(self.nodes)]))
         for edgeIdx in range(len(self.edges)):
             edge = self.edges[edgeIdx]
-            matB[edgeIdx, self.nodesIdx[edge[0]]] = -1
-            matB[edgeIdx, self.nodesIdx[edge[1]]] = 1
-        matB[len(self.edges), self.nodesIdx[node1]] = -1
-        matB[len(self.edges), self.nodesIdx[node2]] = 1
+            matB[edgeIdx, self.nodesIdx[edge[0]]] = 1
+            matB[edgeIdx, self.nodesIdx[edge[1]]] = -1
+        matB[len(self.edges), self.nodesIdx[node1]] = 1
+        matB[len(self.edges), self.nodesIdx[node2]] = -1
         
         return matB
         
-    def solveLinSystem(self, node1, node2):
+    def solveLinSystem(self, node1, node2, batteryVol=1.0):
         # vector: (currents, voltages, potentials)
         # # unknowns: (len(self.edges)+1, len(self.edges)+1, len(self.nodes)-2)
         matB = self.calculateIncidenceMatrix(node1, node2)
-        node1Idx = self.nodesIdx[node1]
+        # node1Idx = self.nodesIdx[node1]
         node2Idx = self.nodesIdx[node2]
         
         # square matrix
-        ndim = 2 * len(self.edges) + len(self.nodes)
+        ndim = 2 * len(self.edges) + len(self.nodes) + 1
         matM = np.matrix(np.zeros([ndim, ndim]))
         vecb = np.zeros(ndim)
         
-        # B*I = 0
-        # number of eqns: len(self.nodes)
-        for i in range(len(self.nodes)):
+        # B*I = 0  (excluding the grounded node)
+        # number of eqns: len(self.nodes)-1
+        for i in range(len(self.nodes)-1):
             for j in range(len(self.edges)+1):
                 matM[i, j] = matB[j, i]
                 
-        # BP-V = 0
-        # number of eqns: len(self.edges)
-        for i, di in zip(range(len(self.edges)), 
-                         range(len(self.nodes), 
-                               len(self.nodes)+len(self.edges))):
-            matM[di, i+len(self.edges)+1] = -1.
-            for j in range(len(self.nodes)):
-                if j == node1Idx or j == node2Idx:
-                    vecb[di] = matB[i, j] if j == node1Idx else 0
-                else:
-                    pjzero = 2 * len(self.edges) + 2
-                    pj = pjzero + j
-                    pj -= 1 if j > node1Idx else 0
-                    pj -= 1 if j > node2Idx else 0
-                    matM[di, pj] = matB[i, j]
+        # BP-V = 0  (including the battery)
+        # number of eqns: len(self.edges)+1
+        for di in range(len(self.edges)+1):
+            i = len(self.nodes) - 1 + di
+            if di != len(self.edges):
+                j = len(self.edges) + 1 + di
+                matM[i, j] = -1.
+            else:
+                vecb[len(self.nodes) + len(self.edges) - 1] = -1.
+            for dj in range(len(self.nodes)):
+                if dj != node2Idx:
+                    j = 2 * len(self.edges) + 1 + dj
+                    j -= 1 if dj > node2Idx else 0
+                    matM[i, j] = matB[di, dj]
                     
-        # IR-V = 0
-        # number of eqns: len(self.edges)
-        for i in range(len(self.edges)):
-            di = len(self.nodes) + len(self.edges) + i
-            vi = len(self.edges) + 1 + i
-            matM[di, i] = self.edges[i][2]
-            matM[di, vi] = -1.
-            
-        return matM
+        # IR-V = 0   (including the battery)
+        # number of eqns: len(self.edges)+1
+        for di in range(len(self.edges)):
+            i = len(self.nodes) + len(self.edges) + di
+            ij = di
+            vj = len(self.edges) + 1 + di
+            matM[i, ij] = self.edges[di][2]
+            matM[i, vj] = -1.
+        matM[ndim-1, len(self.edges)] = 1.
+        matM[ndim-1, ndim-1] = -batteryVol
+        
+        return np.array(matM), vecb
         
